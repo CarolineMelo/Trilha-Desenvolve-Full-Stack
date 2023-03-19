0ff7c955cd0591855fff544e84736f7ed04f9d32
Caroline <2015302@aluno.univesp.br>
2023-03-18 22:08:51 -0300

    JavaScript: objetos

---
+++ JavaScript/javaScript-objeto/treinando-spread-operator.js
@@ -0,0 +1,32 @@
+//Spread Operator permite espalhar as informações de um array de objetos em um outro array. 
+
+
+const clientes = [
+    {
+      nome: "André",
+      cpf: "12312312312",
+      dependentes: [{
+        nome: "Sara",
+        parentesco: "filha",
+        dataNasc: "20/03/2011"
+       },
+       {
+        nome: "Samia",
+        parentesco: "filha",
+        dataNasc: "04/01/2014"
+       }],
+    },
+    {
+      nome: "Juliana",
+      cpf: "56767867867",
+      dependentes: [{
+        nome: "Sophia",
+        parentesco: "filha",
+        dataNasc: "30/08/2020"
+       }],
+    }
+   ]
+
+
+const listaDependentes = [...clientes[0].dependentes, ...clientes[1].dependentes]
+console.table(listaDependentes)
