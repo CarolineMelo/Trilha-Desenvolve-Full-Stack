f9b456dade88b6d917dc0ffed813fa70b5867691
Caroline <2015302@aluno.univesp.br>
2023-04-09 13:20:30 -0300

    testando o acesso a atributos privados

--- JavaScript/funcoes-e-prototipos/Admin.js
+++ JavaScript/funcoes-e-prototipos/Admin.js
@@ -4,13 +4,16 @@ export default class Admin extends User {
     constructor(nome, email, nascimento, role = 'admin', ativo=true) {
         super(nome, email, nascimento, role, ativo)
     }
+
+   
     criarCurso(nomeDoCurso, vagas) {
         return `Curso de ${nomeDoCurso} criado com ${vagas} vagas`
     }
 }
 
 // const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01')
-// console.log(novoAdmin)
+// console.log(novoAdmin.exibirInfos())
+
 // console.log(novoAdmin.criarCurso('Artes',20))
 
 
--- JavaScript/funcoes-e-prototipos/index.js
+++ JavaScript/funcoes-e-prototipos/index.js
@@ -2,8 +2,14 @@ import User from "./User.js";
 import Admin from "./Admin.js";
 import Docente from "./Docente.js";
 
-const novoUser = new User('Maria', 'm@m.com','2021-01-01')
-console.log(novoUser.exibirInfos())
+// const novoUser = new User('Maria', 'm@m.com','2021-01-01')
+// console.log(novoUser.exibirInfos())
+
+const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01')
+// console.log(novoAdmin.exibirInfos())
+//console.log(novoAdmin.nomeAdmin())
+console.log(novoAdmin.nome)
+
 
 // novoUser.#nome = 'Bela'
 // console.log(novoUser.nome)
