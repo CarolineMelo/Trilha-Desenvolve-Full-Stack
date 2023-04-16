2e819cc6c22aae3faf89fdd9238b81593e27fd5c
Caroline <2015302@aluno.univesp.br>
2023-04-15 23:41:10 -0300

    Repetição otimizada com For

--- JavaScript/javaScript-para-web-aluramidi/main.js
+++ JavaScript/javaScript-para-web-aluramidi/main.js
@@ -5,19 +5,14 @@ function tocaSom(idElementoAudio) {
 // document.querySelector('.tecla_pom').onclick = tocaSomPom;
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador=0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1];
 
     const idAudio = `#som_${instrumento}`;
-    // console.log(idAudio)
 
     tecla.onclick = function () {
         tocaSom(idAudio);
-    };
-
-    contador +=1;
+    }
     
 }
