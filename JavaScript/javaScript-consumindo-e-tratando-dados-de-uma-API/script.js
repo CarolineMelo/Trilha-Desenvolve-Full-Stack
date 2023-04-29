386ded6cd72556edbd433c1f227b24b52ae21734
Caroline <2015302@aluno.univesp.br>
2023-04-29 19:37:10 -0300

    preparando o ambiente

---
+++ JavaScript/javaScript-consumindo-e-tratando-dados-de-uma-API/script.js
@@ -0,0 +1,9 @@
+console.log('Mandando oi')
+
+function mandaMensagem() {
+    console.log('oi')
+    console.log('Tudo bem?')
+    console.log('Bom dia')
+}
+mandaMensagem();
+console.log('Até mais')
